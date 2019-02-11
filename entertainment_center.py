5f16e156e504a8a769bf71bc20997cc5e12b7df5
VrushaliRN <47481078+VrushaliRN@users.noreply.github.com>
2019-02-11 16:10:50 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,7 @@ import fresh_tomatoes
 import media
 
 
-#This section is to provide the information for each movie in the Movie Class which is in the Media Module.
+"""This section is to provide the information for each movie in the Movie Class which is in the Media Module."""
 
 titanic = media.Movie("Titanic",
                         "A story of two lovers and famous ship",
@@ -23,13 +23,13 @@ frozen = media.Movie("Frozen",
 
 
 avengers = media.Movie("The Avengers",
-                     "it is a story where all superheroes come together to protect the earth from evil powers",
+                     "story of superheroes come together to protect the earth from evil powers",
                      "http://www.movieposter.com/posters/archive/main/247/MPW-123738",
                      "https://www.youtube.com/watch?v=hA6hldpSTF8")
 
 
 taken = media.Movie("Taken",
-                    "a former CIA operative who sets about tracking down his teenage daughter Kim and her best friend Amanda after the two girls are kidnapped by Albanian sex traffickers while traveling in France during a vacation.",
+                    "story of a CIA agent and about his operations",
                     "http://www.movieposter.com/posters/archive/main/154/MPW-77018",
                     "https://www.youtube.com/watch?v=3Tz9tQr1Zgo")
 
@@ -41,8 +41,8 @@ annabelle = media.Movie("Annabelle",
 
                         
 
-#"import fresh_tomatoes" allows this code to turn into a movie website by using the function "open_movies_page"  
-#The "open_movies_page" function takes a list of movies, then creates and opens
-#an html webpage or website that shows those (given) movies
-movies = [titanic, aquaman, frozen, avengers, taken, annabelle]
+"""import fresh_tomatoes" allows this code to turn into a movie website by using the function "open_movies_page"  
+The "open_movies_page" function takes a list of movies, then creates and opens
+an html webpage or website that shows those (given) movies"""
+movies = [titanic, aquaman, frozen, avengers, taken, annabelle] 
 fresh_tomatoes.open_movies_page(movies)
