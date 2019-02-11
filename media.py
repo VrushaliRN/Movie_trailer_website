589589e133551386978580fd19175e1e4efe81f8
VrushaliRN <47481078+VrushaliRN@users.noreply.github.com>
2019-02-11 16:11:25 +0530

    Update media.py

--- media.py
+++ media.py
@@ -6,13 +6,13 @@ class Movie:
       
     #Initializing and creating space in memory for what we are creating in this movie_website_trailer_project
     #"selfmade" is the object being created
-    def __init__(selfmade, the_movie_title, movie_storyline, image_poster, youtube_trailer):
-        selfmade.title = the_movie_title
-        selfmade.storyline = movie_storyline
-        selfmade.image_poster_url = image_poster
-        selfmade.trailer_youtube_url = youtube_trailer
+    def __init__(self, the_movie_title, movie_storyline, image_poster, youtube_trailer):
+        self.title = the_movie_title
+        self.storyline = movie_storyline
+        self.image_poster_url = image_poster
+        self.trailer_youtube_url = youtube_trailer
 
     #instance method is here:
-    def show_trailer(selfmade):
-        webbrowser.open(selfmade.youtube_url_trailer)
+    def show_trailer(self):
+        webbrowser.open(self.youtube_url_trailer)
         
